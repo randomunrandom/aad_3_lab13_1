05513e8b0a93cbeb638e2350b65d9fbd34424219
Random <randomdanilka@gmail.com>
2018-12-24 20:34:16 +0300

    changed input method

--- arr.cpp
+++ arr.cpp
@@ -6,7 +6,7 @@
 
 bool arr::is_number_prime(int num) {
     bool is_prime=true;
-    if (num < 0) return false;
+    if (num <= 0) return false;
     for(int i = 2; i <= num / 2; ++i) {
         if(num % i == 0) {
             is_prime = false;
@@ -31,25 +31,3 @@ void arr::data(int nn, int *aa) {
     }
 }
 
-void arr::main() {
-    int n, *a;
-    cout << "программа находит элементы массива с индексами-натуральными числами и обнуляет их" << endl;
-    cout << "введите размер массива(n): ";
-    cin >> n;
-    while(n <= 0) {
-        cout << "введите натуральное число: ";
-        cin >> n;
-    }
-    a = new int[n];
-    for (int i = 0; i < n; ++i) {
-        cout << "введите " << i << "-й элемент массива: ";
-        cin >> a[i];
-    }
-    arr el(n, a);
-    this->reset_prime_indexes();
-    this->data(n, a);
-    for (int i = 0; i < n; ++i) {
-        cout << a[i] << " ";
-    }
-    cout << endl;
-}
